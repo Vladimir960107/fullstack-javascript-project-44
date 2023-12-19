523d252381feef4aac149317a156a8eb51b479ee
Vladimir <loginov.volodia@gmail.com>
2023-12-19 16:19:41 +0300

    Hexlet step 5 development, minor issues with maintability, try 1

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,11 @@
 import readlineSync from 'readline-sync';
 
-const MINRANDOMINT = 1;
-const MAXRANDOMINT = 100;
+const MIN_RANDOM_INT = 1;
+const MAX_RANDOM_INT = 100;
 const NUMBEROFRIGHTANSWERSBEFOREEXIT = 3;
+const GREETING_MESSAGE = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const getRandomArbitraryInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const GetRandomArbitraryInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 const IsNumberEvenCheckerYesOrNo = (number) => {
   if (number % 2 === 0) {
     return 'yes';
@@ -14,24 +15,30 @@ const IsNumberEvenCheckerYesOrNo = (number) => {
   return null;// For cases where the number is not an integer
 };
 
+const playRound = (counter) => {
+  const randInt = GetRandomArbitraryInt(MIN_RANDOM_INT, MAX_RANDOM_INT);
+  console.log(`Question: ${randInt}`);
+  const answer = readlineSync.question('Your answer: ');
+  const correctAnswer = IsNumberEvenCheckerYesOrNo(randInt);
+
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+    return counter + 1;
+  }
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+  return -1;
+};
+
 const EvenGame = (name = '') => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log(GREETING_MESSAGE);
   let counterOfRightAnswers = 0;
   while (counterOfRightAnswers < NUMBEROFRIGHTANSWERSBEFOREEXIT) {
-    const randInt = getRandomArbitraryInt(MINRANDOMINT, MAXRANDOMINT);
-    console.log(`Question: ${randInt}`);
-    const answer = readlineSync.question('Your answer: ');
-    const correntAnswer = IsNumberEvenCheckerYesOrNo(randInt);
-    if (answer === correntAnswer) {
-      console.log('Corect!');
-      counterOfRightAnswers += 1;
-      if (counterOfRightAnswers === NUMBEROFRIGHTANSWERSBEFOREEXIT) {
-        console.log(`Congratulations, ${name}`);
-      }
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correntAnswer}'`);
-      console.log(`let's try again ${name}`);
+    counterOfRightAnswers = playRound(counterOfRightAnswers);
+    if (counterOfRightAnswers === -1) {
+      console.log(`Let's try again, ${name}`);
       break;
+    } else if (counterOfRightAnswers === NUMBEROFRIGHTANSWERSBEFOREEXIT) {
+      console.log(`Congratulations, ${name}`);
     }
   }
 };
