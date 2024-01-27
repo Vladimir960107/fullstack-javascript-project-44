c8b98bb367297662f3a0e690f46cfc3f8a7b89df
Vladimir <loginov.volodia@gmail.com>
2024-01-27 15:23:30 +0300

    askNameAndGreed was refactored 2

--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,6 @@ const AskNameAndGreet = () => {
   console.log('Welcome to the Brain Games! \n');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name} `);
+  return name;
 };
 export default AskNameAndGreet;
