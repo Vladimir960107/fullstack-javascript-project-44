8a584414c361fab8f1fba1400d3e6bb659973ebe
Vladimir <loginov.volodia@gmail.com>
2024-01-27 15:17:36 +0300

    askNameAndGreet was refactored

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const AskNameAndGreet = () => {
-  console.log('Welcome to the Brain Games!');
+  console.log('Welcome to the Brain Games! \n');
   const name = readlineSync.question('May I have your name?: ');
-  console.log(`Hello ${name} `);
+  console.log(`Hello, ${name} `);
 };
 export default AskNameAndGreet;
