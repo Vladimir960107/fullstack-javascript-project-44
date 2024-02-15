5c9c2a3c3ed78212a753a66590c942290a27bdb5
Vladimir <loginov.volodia@gmail.com>
2024-02-15 14:35:22 +0300

    progression.js progression fixed

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,7 +3,7 @@ import generateNumber from '../utils.js';
 
 const MIN_RANDOM_INT_PROG_START = 1;
 const MAX_RANDOM_INT_PROG_START = 10;
-const RECOMMENDED_PROG_LENGTH = 10;
+const PROG_LENGTH = 10;
 
 // Начальное сообщение игры
 export const description = 'What number is missing in the progression?';
@@ -12,9 +12,9 @@ export const description = 'What number is missing in the progression?';
 export const generateRound = () => {
   const startNumber = generateNumber(MIN_RANDOM_INT_PROG_START, MAX_RANDOM_INT_PROG_START);
   const step = generateNumber(MIN_RANDOM_INT_PROG_START, MAX_RANDOM_INT_PROG_START);
-  const hiddenIndex = generateNumber(0, RECOMMENDED_PROG_LENGTH - 1);
+  const hiddenIndex = generateNumber(0, PROG_LENGTH - 1);
 
-  const progression = Array.from({ RECOMMENDED_PROG_LENGTH }, (_, i) => startNumber + step * i);
+  const progression = Array.from({ length: PROG_LENGTH }, (_, i) => startNumber + step * i);
   const correctAnswer = progression[hiddenIndex].toString();
   progression.splice(hiddenIndex, 1, '..').toString();
   return [progression, correctAnswer];
