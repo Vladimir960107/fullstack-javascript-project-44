df11937cf4bca42a0197cdba4a440f600a2b7ad4
Vladimir <loginov.volodia@gmail.com>
2024-02-18 10:01:38 +0300

    export default to export in even.js file

--- src/games/even.js
+++ src/games/even.js
@@ -8,13 +8,11 @@ const MAX_RANDOM_INT_EVEN = 100;
 const isEven = (number) => number % 2 === 0;
 
 // Функция для вывода начального сообщения игры
-const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+export const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 // Функция для проведения раунда игры
-const generateRound = () => {
+export const generateRound = () => {
   const question = generateNumber(MIN_RANDOM_INT_EVEN, MAX_RANDOM_INT_EVEN);
   const correctAnswer = isEven(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
-
-export default { description, generateRound };
